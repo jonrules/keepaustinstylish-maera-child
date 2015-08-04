b61851e7d5c12bf670dea3b27d2e0ff1b216e0ac
Jonathan Baltazar <jonrules@gmail.com>
2015-08-04 15:15:40 -0500

    add timestamp query string to logo svg

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,10 @@ jQuery(function ($) {
 	$logo.attr('data-original-src', $logo.attr('src'));
 	$logo.hover(
 		function () {
-			this.src = this.getAttribute('data-original-src').replace('-WHT', '-BLK');
+			this.src = this.getAttribute('data-original-src').replace('-WHT', '-BLK') + '?time=' + (new Date()).getMilliseconds();
 		},
 		function () {
-			this.src = this.getAttribute('data-original-src');
+			this.src = this.getAttribute('data-original-src') + '?time=' + (new Date()).getMilliseconds();
 		}
 	);
 });
